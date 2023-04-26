e8e2f144c1458c5889ec4773d453d299ab250ebc
Ruqayyah2443 <ruqayyahadebanjo22@gmail.com>
2023-04-26 09:05:59 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,25 +3,26 @@
 /**
  * print_alphabet_x10 - function that print 10x alphabets
  *
- * Description: Write a function that prints 10 times the alphabet, in lowercase, followed by a new line
- *
- * Return: void
  */
+
 void print_alphabet_x10(void)
 {
-	int round = 0;
-	char letter = 'a';
+	char ch;
 
-	while (round < 10)
+	int i;
+
+	i = 0;
+
+	while (i < 10)
 	{
-		letter = 'a';
-		while (letter <= 'z')
+		ch = 'a';
+		while (i <= 'z')
 		{
-			_putchar(letter)
-			letter++;
+			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
 
-		round++;
+		i++;
 	}
 }
