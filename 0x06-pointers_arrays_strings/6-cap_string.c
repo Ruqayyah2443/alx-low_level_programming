73c6017db3168bcbfff413493e05c88ee340ccb1
Ruqayyah2443 <ruqayyahadebanjo22@gmail.com>
2023-05-05 18:48:30 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,27 +2,33 @@
 
 /**
  * cap_string - Capitalize all words of a string.
- * @str: The string to be capitalized.
- * Return: Apointer to the changed string.
+ * @n: The string to be capitalized.
+ * Return: caps on first letter of a seperator
  */
 
-char *cap_string(char *)
+char *cap_string(char *n)
 {
-	int index = 0;
-
-	while (str[index])
+	int i, x;
+	int cap = 32;
+	int seperators[] = {',', ';', '.', '?', '"', '(', ')',
+		'{', '}', ' ', '\n', '\t'};
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a' && str[index] <='z'))
-			
-			index++;
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - cap;
+		}
 
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index – 1] == ‘\n’ ||
-		str[index – 1] == ‘,’ ||
-		str[index – 1] == ‘;’ ||
-		str[index – 1] == ‘.’ ||
-		str[index – 1] == ‘!’ ||
-		str[index – 1] == ‘?’ ||
-		str[index – 1] == ‘"' ||
-		str[index – 1] == ‘\n||)
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (n[i] == seperators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (n);
+}
