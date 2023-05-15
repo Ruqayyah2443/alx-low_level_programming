dc244eb6204726992810064c41ae10bd35c23730
Ruqayyah2443 <ruqayyahadebanjo22@gmail.com>
2023-05-15 09:33:18 -0700

    task 0

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare string
+ * @s1: string
+ * @s2: strmp
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
